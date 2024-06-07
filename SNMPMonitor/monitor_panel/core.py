1723b4df0a646cf6324025970d7b802a2b522c4a
danil-226 <kosorev.daniel@gmail.com>
2024-06-07 13:03:47 +0300

    prepare core for integration

--- SNMPMonitor/monitor_panel/core.py
+++ SNMPMonitor/monitor_panel/core.py
@@ -1,7 +1,7 @@
 """
 Contains SNMPmonitor class
 """
-from os import get_terminal_size, system
+from os import get_terminal_size, system, path
 from time import sleep
 from threading import Thread
 from art import tprint
@@ -11,29 +11,34 @@ from pysnmp.carrier.asyncore.dgram import udp
 from pysnmp.entity import config
 from pysnmp.entity.rfc3413 import ntfrcv
 from pysnmp.hlapi import *
-from monitor.panel.models import Host
+from monitor_panel.models import Host
+from monitor_panel.hosts import HOSTS
+from json import load
+from uuid import getnode
 
-TERM_WIDTH = get_terminal_size().columns
 
-
-class SNMPmonitor:
+class MonitorCore:
     """
     Network-monitoring system
     """
 
     def __init__(self):
-        tprint("SNMP Monitor")
+        with open(path.join('./', 'SNMPMonitor', 'data.json')) as json_file:
+            data = json.load(json_file)
+            if data['filter_subnet']:
+                self.SUBNET = data['subnet']
         self.devices = []
-        self.host = Host()
+        self.host = None
+        self.params = []
         for interface in interfaces():
             for link in ifaddresses(interface)[AF_INET]:
-                if '127' not in link['addr'] and '172' not in link['addr']:
-                    mask = str(sum([(sum(list(map(int, i))))
-                                    for i in [format(int(i), 'b')
-                                              for i in link['netmask'].split('.') if
-                                              i != '0']]))
-                    self.host.interfaces.append(
-                        Iface(f"{link['addr']}/{mask}"))
+                if f'.{self.SUBNET}.' in link['addr']:
+                    self.params['ip'] = link['addr']
+                if link['mac'] == ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
+                                            for ele in range(0, 8 * 6, 8)][::-1]):
+                    self.params['role'] = 'Manager'
+                else:
+                    self.params['role'] = 'Agent'
         self.find_devs()
 
     def find_devs(self):
@@ -41,43 +46,30 @@ class SNMPmonitor:
         Detect active devices in network
         :return:
         """
-        system('clear')
-        tprint("SNMP Monitor (Beta)")
-        print("Scanning network...")
         scanner = PortScanner()
+        devs = []
+        i = 0
         for eth in self.host.interfaces:
+            params.append(dict())
             addr, mask = eth.address.split('/')
             ip_range = f"{addr[:addr.rfind('.')]}.0/{mask}"
             scanner.scan(hosts=ip_range, arguments='-sn')
             for iface in scanner.all_hosts():
-                eths = [Iface(iface, scanner[addr]['status']['state'])]
-                self.devices.append(Host(interfaces=eths, status="Active"))
+                params[i]['status'] = scanner[addr]['status']['state']
+                devs.append(iface)
+                i += 1
 
-        for dev in self.devices:
-            for eth in dev.interfaces:
-                print(f" - New device({eth})")
-                data = self.snmp_get(eth.address, '1.3.6.1.2.1.1.1.0')
-                if data:
-                    dev.hostname = data[3]
-                    dev.system = data[5]
-                else:
-                    dev.status = 'Unreachable'
-
-        for dev in self.devices:
-            for _dev in self.devices:
-                if dev != _dev:
-                    if dev.hostname == _dev.hostname:
-                        dev.interfaces += _dev.interfaces
-                        self.devices.remove(_dev)
-        sleep(10)
-        system('clear')
-        tprint("SNMP Monitor (Beta)")
-        print(f"\n{'=' * TERM_WIDTH}")
-        for dev in self.devices:
-            if dev.hostname == 'manager':
-                print(f"|| LOCAL: {dev} \n{'=' * TERM_WIDTH}")
+        for dev in self.params:
+            data = self.snmp_get(dev['ip'], '1.3.6.1.2.1.1.1.0')
+            if data:
+                dev['status'] = data[3]
+                dev['system'] = data[5]
             else:
-                print(f"|| REMOTE: {dev} \n{'=' * TERM_WIDTH}")
+                dev['status'] = 'Inactive'
+            self.devices.append(Host(ip=self.params['ip'], hostname=self.params['hostname'], role=self.params['role'],
+                                     system=self.params['system'], status=self.params['status']))
+            for i in self.devices:
+                HOSTS.append(i)
 
     @staticmethod
     def snmp_get(ip_addr: str, oid: str) -> list:
@@ -87,7 +79,6 @@ class SNMPmonitor:
         :param oid: snmp object identifier
         :return:
         """
-        print('\tGetting information...')
         error_indication, error_status, error_index, var_binds = next(
             getCmd(SnmpEngine(),
                    CommunityData('test'),
@@ -96,10 +87,8 @@ class SNMPmonitor:
                    ObjectType(ObjectIdentity(oid)))
         )
         if len(var_binds) > 0:
-            print('\tDone')
             return var_binds[0].prettyPrint().split(' ')
         else:
-            print('\tNo information')
             return []
 
     @staticmethod
@@ -137,17 +126,16 @@ class SNMPmonitor:
         :param cb_ctx:
         :return:
         """
-        print("Received new Trap message")
-        for name, val in var_binds:
-            print(f"{name.prettyPrint()} = {val.prettyPrint()}")
+        if SNMPmonitor.listen_trap(snmp_engine):
+            for name, val in var_binds:
+                return name.prettyPrint(), val.prettyPrint()
 
     @staticmethod
-    def listen_trap():
+    def listen_trap(snmp_engine):
         """
         Start snmp-trap listener
         :return:
         """
-        snmp_engine = SnmpEngine()
         config.addTransport(
             snmp_engine,
             udp.domainName,
@@ -157,32 +145,36 @@ class SNMPmonitor:
         config.addV1System(snmp_engine, 'test-area', 'test')
         ntfrcv.NotificationReceiver(snmp_engine, SNMPmonitor.operate_trap)
         snmp_engine.transportDispatcher.jobStarted(1)
-        print(
-            f"Listening for incoming SNMP TRAP messages...\n\n{'=' * TERM_WIDTH}")
-        snmp_engine.transportDispatcher.runDispatcher()
+        return snmp_engine.transportDispatcher.runDispatcher()
 
-    def run(self):
+    def update_devs(self):
         """
         Start network monitoring
         :return:
         """
-        print("\nMonitoring remote hosts...")
-        trap_th = Thread(target=self.listen_trap)
-        trap_th.start()
-        while True:
-            for dev in self.devices:
-                cpu_info = self.snmp_walk(
-                    dev.interfaces[0].address, '1.3.6.1.2.1.25.3.3.1.2')
-                network_info = self.snmp_walk(
-                    dev.interfaces[0].address, '1.3.6.1.2.1.2.2.1.10')
-                if cpu_info and network_info:
-                    print(f"{dev.hostname}({dev.interfaces[0].address}):")
-                    print(f"\tTotal bytes received: {network_info}")
-                    print(f"\tCPU usage: {cpu_info}%")
-                    print('=' * TERM_WIDTH)
-            sleep(10)
+        current_time = datetime.datetime.now()
+        for dev in self.params:
+            dev['cpu_info'] = self.snmp_walk(
+                dev['ip'], '1.3.6.1.2.1.25.3.3.1.2')
+            dev['hdd_info'] = self.snmp_walk(
+                dev['ip'], '1.3.6.1.2.1.16.2.4.1')
+            dev['hdd_total'] = self.snmp_walk(
+                dev['ip'], '1.3.6.1.2.1.16.2.4.0')
+            dev['network_info'] = self.snmp_walk(
+                dev['ip'], '1.3.6.1.2.1.2.2.1.10')
+            dev['status'] = self.snmp_get(
+                dev['ip'], '1.3.6.1.2.1.1.1.0')[3]
+            dev['usb_info'] = self.snmp_walk(
+                dev['ip'], '1.3.6.1.2.1.8.1.3.0')
+            for i in HOSTS:
+                if i.ip == dev[ip]:
+                    i.mesures['cpu_usage']['measures'].append((f'{current_time.hour}:{current_time.minute}',
+                                                               dev['cpu_info']))
+                    i.mesures['hdd_usage']['measures'].append((f'{current_time.hour}:{current_time.minute}',
+                                                               dev['hdd_info']))
+                    i.mesures['network_usage']['measures'].append((f'{current_time.hour}:{current_time.minute}',
+                                                                   dev['network_info']))
+                    i.hdd_total = dev['hdd_total']
+                    i.status = dev['status']
+                    i.usb_devs = dev['usb_info']
 
-
-if __name__ == '__main__':
-    monitor = SNMPmonitor()
-    monitor.run()

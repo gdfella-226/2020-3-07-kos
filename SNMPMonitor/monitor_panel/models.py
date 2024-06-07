a3eb41fd3b2faffa50be674ed36767639fc9da3c
danil-226 <kosorev.daniel@gmail.com>
2024-06-07 11:33:44 +0300

    rearrange hosts

--- SNMPMonitor/monitor_panel/models.py
+++ SNMPMonitor/monitor_panel/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Host(models.Model):
+    ip: list = models.GenericIPAddressField(protocol='IPv4', help_text='IP address of host`s network interface')
+    hostname: str = models.CharField(max_length=20, help_text='Hostname')
+    role: str = models.CharField(default='Agent', max_length=20, help_text='SNMP role: Manager / Agent')
+    status: str = models.BooleanField(default=False, help_text='Device status: Active / Inactive')
+    system: str = models.CharField(max_length=50, help_text='Name & version of host`s OS')
+    hdd_total: int = models.IntegerField(default=50, help_text='Host`s HDD total spase (Gb')
+    usb_devs: int = models.IntegerField(default=0, help_text='Amount of connected USB devices')
+    measures: str = models.TextField(help_text='Collect measures of host`s parameters')
+
+    def update_measures(self, param_val: tuple, critical) -> None:
+        del self.measures[param_val[0]]['measures'][0]
+        self.measures[param_val[0]]['measures'].append(param_val[1])
+        measures[param_val[0]]['critical_value'] = critical
+
+    def check_measures_for_crit(self) -> bool:
+        for param in self.measures:
+            crit = param['critical_value']
+            for measure in param['measures']:
+                if int(measure[1]) >= int(crit):
+                    return True
+        return False
+
--- SNMPMonitor/monitor_panel/views.py
+++ SNMPMonitor/monitor_panel/views.py
@@ -1,43 +1,34 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from monitor_panel.models import Host
+from loguru import logger
+from monitor_panel.hosts import HOSTS
 
 
-trouble_host = '192.168.7.102'
+active_measure = 'hdd_usage'
+active_host = HOSTS[0]
+trouble_host = HOSTS[0]
 show_warning = False
-hosts = [
-            {'ip': '192.168.7.101', 'name': 'HostName1', 'role': 'Manager', 'os': 'Ubuntu 2020.4',
-             'status': 'Active', 'cpu_usage': 30, 'hdd_usage': 32, 'hdd_total': 50, 'network_usage': 50468, 'usb': 0},
-            {'ip': '192.168.7.102', 'name': 'HostName2', 'role': 'Agent', 'os': 'Windows Server 2012',
-             'status': 'Inactive', 'cpu_usage': 30, 'hdd_usage': 32, 'hdd_total': 100, 'network_usage': 9649, 'usb': 0},
-            {'ip': '192.168.7.103', 'name': 'HostName3', 'role': 'Agent', 'os': 'Ubuntu Router',
-             'status': 'Active', 'cpu_usage': 30, 'hdd_usage': 10, 'hdd_total': 30, 'network_usage': 2582542, 'usb': 2},
-            {'ip': '192.168.7.104', 'name': 'HostName4', 'role': 'Agent', 'os': 'IPCam',
-             'status': 'Active', 'cpu_usage': 30, 'hdd_usage': 32, 'hdd_total': 30, 'network_usage': 22684, 'usb': 0},
-            {'ip': '192.168.7.105', 'name': 'HostName5', 'role': 'Agent', 'os': 'Windows 10',
-             'status': 'Active', 'cpu_usage': 30, 'hdd_usage': 32, 'hdd_total': 50, 'network_usage': 5548, 'usb': 2},
-        ]
-active_host_ip = '192.168.7.101'
-active_host = hosts[0]
-critical_value = 75
 
 
 def index(request):
-    global show_warning, active_host_ip, trouble_host, hosts, active_host, critical_value
+    global show_warning, trouble_host, active_host, active_measure
 
     if request.method == 'POST':
         if 'ip' in request.POST:
             active_host_ip = request.POST.get('ip')
-            active_host = [i for i in hosts if i['ip'] == active_host_ip][0]
+            active_host = [i for i in HOSTS if i.ip == active_host_ip][0]
+
         if 'critical_value' in request.POST:
-            critical_value = int(request.POST.get('critical_value'))
+            active_host.measures[active_measure]['critical_value'] = int(request.POST.get('critical_value'))
 
     context = {
         'show_warning': show_warning,
         'trouble_host': trouble_host,
         'title': 'Host disconnected',
-        'description': f'Connection with {trouble_host} has been lost',
-        'hosts': hosts,
+        'description': f'Connection with {trouble_host.hostname} has been lost',
+        'hosts': HOSTS,
         'active_host': active_host
     }
 
@@ -45,18 +36,19 @@ def index(request):
 
 
 def get_chart_data(request):
-    global critical_value
+    global active_host, active_measure
+    labels = [pair[0] for pair in active_host.measures[active_measure]['measures']]
+    values = [pair[1] for pair in active_host.measures[active_measure]['measures']]
     data = {
-        'labels': ['12:35', '12:40', '12:45', '12:50', '12:55', '13:05', '13:10'],
-        'values': [65, 59, 80, 81, 56, 55, 40],
-        'critical_value': critical_value,
+        'labels': labels,
+        'values': values,
+        'critical_value': active_host.measures[active_measure]['critical_value'],
     }
     return JsonResponse(data)
 
 
 @csrf_exempt
 def set_active_host(request):
-    global active_host_ip, show_warning
     if request.method == 'POST':
         ip = request.POST.get('ip')
         saved_ip = ip
